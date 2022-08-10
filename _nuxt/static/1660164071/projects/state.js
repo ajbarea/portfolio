9b30480dd313d91a09e610cd852e4a039a2cc4d8
ajbarea <79944464+ajbarea@users.noreply.github.com>
2022-08-10 16:45:33 -0400

    aj
    redeploy nuxt portfolio

---
+++ _nuxt/static/1660164071/projects/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fportfolio\u002F_nuxt\u002Fstatic\u002F1660164071",layout:"default",error:a,serverRendered:true,routePath:"\u002Fprojects",config:{_app:{basePath:"\u002Fportfolio\u002F",assetsPath:"\u002Fportfolio\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"21264ac6"}}}}(null));
