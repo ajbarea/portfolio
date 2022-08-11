d49a421cf6c4cf0f94cc26903fced9f270c57ba8
ajbarea <79944464+ajbarea@users.noreply.github.com>
2022-08-11 11:27:52 -0400

    aj
    npm run generate

---
+++ _nuxt/static/1660231614/blog/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fportfolio\u002F_nuxt\u002Fstatic\u002F1660231614",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog",config:{_app:{basePath:"\u002Fportfolio\u002F",assetsPath:"\u002Fportfolio\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"9ab5a662"}}}}(null));
