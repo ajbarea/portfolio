ef0e0600fca5643b1744c1cf48902b1909afc41e
ajbarea <79944464+ajbarea@users.noreply.github.com>
2022-08-16 00:01:16 -0400

    npm run generate
    redeploy

---
+++ _nuxt/static/1660622375/blog/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fportfolio\u002F_nuxt\u002Fstatic\u002F1660622375",layout:"default",error:a,serverRendered:true,routePath:"\u002Fblog",config:{_app:{basePath:"\u002Fportfolio\u002F",assetsPath:"\u002Fportfolio\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"0ee11363"}}}}(null));
