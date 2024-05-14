efa9e774387b5edeb456a5665d4a01b9bacdc308
AJ Barea <ajbareaa@gmail.com>
2024-05-14 12:02:05 -0700

    initial commit

---
+++ docs/_nuxt/static/1715713302/projects/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fportfolio\u002F_nuxt\u002Fstatic\u002F1715713302",layout:"default",error:a,serverRendered:true,routePath:"\u002Fprojects",config:{_app:{basePath:"\u002Fportfolio\u002F",assetsPath:"\u002Fportfolio\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"f85a54d0"}}}}(null));
