7e24926a0796a61692ac08fa2a6b0691b8c57b28
AJ Barea <ajbareaa@gmail.com>
2025-03-21 15:06:12 -0400

    chore: Refactor blog page layout and improve styling for better consistency

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,14 +1,15 @@
 <template>
-  <div>
-    <header class="pt-16 pb-9 sm:pb-16 sm:text-center">
-      <h1 class="mb-4 text-8xl sm:text-4xl tracking-tight text-slate-800 font-extrabold dark:text-slate-200">
+  <div class="max-w-3xl px-4 mx-auto sm:px-6 xl:max-w-5xl xl:px-0">
+    <div class="pt-6 pb-8 space-y-2 md:space-y-5">
+      <h1
+        class="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
         Blog
       </h1>
-      <p class="text-lg text-slate-700 dark:text-slate-400">
+      <p class="text-lg leading-7 text-gray-500 dark:text-gray-400">
         Structured thoughts, notes, resources, and recommendations.
       </p>
-    </header>
-    <div class="space-y-16 mx-auto max-w-7xl">
+    </div>
+    <div class="space-y-16">
       <blog-item v-for="article in articles" :key="article.title" :title="article.title"
         :description="article.description" :date="article.date" :slug="article.slug"></blog-item>
     </div>
